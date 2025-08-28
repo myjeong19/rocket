c07fc8c2117ac0ed927d485c2c98fa4137b4da4c
myjeong19 <myjeong19@naver.com>
2025-08-28 14:35:25 +0900

    Fix. 모바일 반응형

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: '로켓 AI',
   description: '로켓 AI',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
 };
 
 export default function RootLayout({
